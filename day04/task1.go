3f13cb4e70168eec26e361f7250be6fa2bc4a4dc
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-04 13:50:17 +0000

    Move winconditions to common.go file

--- day04/task1.go
+++ day04/task1.go
@@ -17,16 +17,3 @@ func task1(fileData []string) (int, error) {
 
 	return 0, errors.New("none of the boards won :(")
 }
-
-var winConditions = []uint{
-	0b1111100000000000000000000, // first row
-	0b0000011111000000000000000, // second row
-	0b0000000000111110000000000, // third row
-	0b0000000000000001111100000, // fourth row
-	0b0000000000000000000011111, // fifth row
-	0b0000100001000010000100001, // last column
-	0b0001000010000100001000010, // fourth column
-	0b0010000100001000010000100, // third column
-	0b0100001000010000100001000, // second column
-	0b1000010000100001000010000, // first column
-}
