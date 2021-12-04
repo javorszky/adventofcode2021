60672b7caf060fc58bdf6d69eb140beddc7b08c8
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-04 14:04:05 +0000

    Add day 4 link to main

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/javorszky/adventofcode2021/day01"
+	"github.com/javorszky/adventofcode2021/day02"
+	"github.com/javorszky/adventofcode2021/day03"
 	"github.com/javorszky/adventofcode2021/day04"
 )
 
 func main() {
 	fmt.Println("Welcome to Advent of Code 2021",
 		"by Gabor Javorszky")
-	//
-	//day01.Tasks()
-	//day02.Tasks()
-	//day03.Tasks()
+
+	day01.Tasks()
+	day02.Tasks()
+	day03.Tasks()
 	day04.Tasks()
 }
