6bcd8125b28a598177d1bf4559071e670a6bb56f
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-05 18:31:28 +0000

    Move maplines to common and amend

--- day05/common.go
+++ day05/common.go
@@ -70,3 +70,72 @@ func convertToUints(matches []string) []uint {
 
 	return reInts
 }
+
+func mapLines(tuples []tuple) map[uint]uint {
+	m := make(map[uint]uint)
+
+	for _, t := range tuples {
+		switch {
+		case t[0][0] == t[1][0] && t[0][1] < t[1][1]:
+			// first coördinate is the same and first tuple is smaller
+			x := t[0][0] << bitSizeForCoordinates
+
+			for i := t[0][1]; i <= t[1][1]; i++ {
+				m[x|i]++
+			}
+		case t[0][0] == t[1][0] && t[0][1] > t[1][1]:
+			// first coördinate is the same and second tuple is smaller
+			x := t[0][0] << bitSizeForCoordinates
+
+			for i := t[1][1]; i <= t[0][1]; i++ {
+				m[x|i]++
+			}
+		case t[0][0] < t[1][0] && t[0][1] == t[1][1]:
+			// second coördinate is the same, first tuple is smaller
+			y := t[0][1]
+
+			for i := t[0][0]; i <= t[1][0]; i++ {
+				m[(i<<bitSizeForCoordinates)|y]++
+			}
+		case t[0][0] > t[1][0] && t[0][1] == t[1][1]:
+			// second coördinate is the same, second tuple is smaller
+			y := t[0][1]
+
+			for i := t[1][0]; i <= t[0][0]; i++ {
+				m[(i<<bitSizeForCoordinates)|y]++
+			}
+		case t[0][0] < t[1][0] && t[0][1] < t[1][1]:
+			// top left to bottom right
+			for i := uint(0); i <= (t[1][0] - t[0][0]); i++ {
+				x := t[0][0] + i
+				y := t[0][1] + i
+				m[x<<10|y]++
+			}
+		case t[0][0] < t[1][0] && t[0][1] > t[1][1]:
+			// bottom left to top right
+			for i := uint(0); i <= (t[1][0] - t[0][0]); i++ {
+				x := t[0][0] + i
+				y := t[0][1] - i
+				m[x<<10|y]++
+			}
+		case t[0][0] > t[1][0] && t[0][1] < t[1][1]:
+			// top right to bottom left
+			for i := uint(0); i <= (t[1][0] - t[0][0]); i++ {
+				x := t[0][0] - i
+				y := t[0][1] + i
+				m[x<<10|y]++
+			}
+		case t[0][0] > t[1][0] && t[0][1] > t[1][1]:
+			// bottom right to top left
+			for i := uint(0); i <= (t[1][0] - t[0][0]); i++ {
+				x := t[0][0] - i
+				y := t[0][1] - i
+				m[x<<10|y]++
+			}
+		default:
+			log.Fatalf("well something went wrong, neither the first, nor the second coordinates were equal")
+		}
+	}
+
+	return m
+}
