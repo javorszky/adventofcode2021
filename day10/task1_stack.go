39c7a1c263ae716335a9a97d6077934b22abffb7
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-10 22:25:41 +0000

    Further optimize the code to find the invalid syntax

--- day10/task1_stack.go
+++ day10/task1_stack.go
@@ -1,41 +1,46 @@
 package day10
 
-import "log"
-
-var pairs = map[uint8]uint8{
+var pairs = map[int32]int32{
 	0x28: 0x29, // (:)
 	0x5b: 0x5d, // [:]
 	0x7b: 0x7d, // {:}
 	0x3c: 0x3e, // <:>
 }
 
+// ): 3 points.
+// ]: 57 points.
+// }: 1197 points.
+// >: 25137 points.
+var invalidScoreStack = map[int32]int{
+	0x29: 3,     // )
+	0x5d: 57,    // ]
+	0x7d: 1197,  // }
+	0x3e: 25137, // >
+}
+
 func task1Stack(input []string) int {
 	acc := 0
+	stack := make([]int32, 0, 30)
 
 	for _, line := range input {
-		stack := make([]uint8, 0)
+		stack = stack[:0]
 
-		for i := 0; i < len(line); i++ {
-			closing, ok := pairs[line[i]]
-			if ok {
-				stack = append(stack, closing)
+	Loop:
+		for _, ch := range line {
+			switch closing := pairs[ch]; closing {
+			case 0:
+				//if len(stack) == 0 || stack[len(stack)-1] != ch {
+				if stack[len(stack)-1] != ch {
+					acc += invalidScoreStack[ch]
 
-				continue
-			}
+					break Loop
+				}
 
-			if len(stack) == 0 || stack[len(stack)-1] != line[i] {
-				acc += invalidScore[line[i]]
-
-				break
-			}
-
-			if stack[len(stack)-1] == line[i] {
 				stack = stack[:len(stack)-1]
-
-				continue
+			default:
+				// we found the character, which means it was an opening character, so let's stick a closing one here.
+				stack = append(stack, closing)
 			}
-
-			log.Fatalf("entirely unknown character [%s]. This should not have happened", string(line[i]))
 		}
 	}
 
