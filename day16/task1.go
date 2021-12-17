79f22561d1a2da1e66660ef477cded4ddc9bf87f
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-17 10:41:03 +0000

    Add structs for the two types

--- day16/task1.go
+++ day16/task1.go
@@ -1,5 +1,51 @@
 package day16
 
+import "strings"
+
+type lengthType int
+
+const (
+	unknownLengthType lengthType = iota
+	subPacketLength
+	subPacketNumber
+)
+
 func task1(input string) interface{} {
-	return input
+	reader := strings.NewReader(input)
+
+	return reader
+}
+
+type packet interface {
+	Version() int
+	Type() int
+}
+
+type literal struct {
+	packetVersion int
+	packetType    int
+	value         int
+}
+
+func (l literal) Version() int {
+	return l.packetVersion
+}
+
+func (l literal) Type() int {
+	return l.packetType
+}
+
+type operator struct {
+	packetVersion int
+	packetType    int
+	lengthTypeID  lengthType
+	subPackets    []packet
+}
+
+func (o operator) Version() int {
+	return o.packetVersion
+}
+
+func (o operator) Type() int {
+	return o.packetType
 }
