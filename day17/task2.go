1b50c6681f79875065a18cada02ec823e31596d9
Gabor Javorszky <gabor.javorszky@ardanlabs.com>
2021-12-20 22:54:38 +0000

    Day 17 task 2

--- day17/task2.go
+++ day17/task2.go
@@ -1,7 +1,6 @@
 package day17
 
 import (
-	"fmt"
 	"sort"
 )
 
@@ -13,13 +12,9 @@ func task2(coords []int) int {
 }
 
 func getFiringSolutions(xMin, xMax, yMin, yMax int) [][2]int {
+	firingSolutions := make(map[int]map[int]struct{})
 	xTargetSpeeds := getXTargetSpeeds(xMin, xMax)
 	yTargetSpeeds := getYTargetSpeeds(yMin, yMax)
-
-	//fmt.Printf("xspeeds:\n\n%#v\n\n", xTargetSpeeds)
-	//fmt.Printf("yspeeds:\n\n%#v\n\n", yTargetSpeeds)
-
-	firingSolutions := make(map[int]map[int]struct{})
 	firingSolutions, scrubbed := getRightDownShots(firingSolutions, xTargetSpeeds, yTargetSpeeds)
 	firingSolutions = getRightUpShotsNotScrubbed(firingSolutions, scrubbed, xTargetSpeeds, yTargetSpeeds)
 	firingSolutions = getScrubbed(firingSolutions, yTargetSpeeds, scrubbed)
@@ -31,8 +26,6 @@ func getFiringSolutions(xMin, xMax, yMin, yMax int) [][2]int {
 			firingSolutionsButPairs = append(firingSolutionsButPairs, [2]int{x, y})
 		}
 	}
-
-	//fmt.Printf("Firing solutions:\n\n%#v\n\n", firingSolutionsButPairs)
 
 	return firingSolutionsButPairs
 }
@@ -148,13 +141,13 @@ func getRightUpShotsNotScrubbed(
 	sort.Ints(scrubbed)
 
 	maxScrubbed := scrubbed[len(scrubbed)-1]
-	maxInitialVelocity := (maxScrubbed - 1) / 2
+	maxInitialVelocity := -((maxScrubbed - 1) / 2)
 
 	// speed, step, upright velocity. We check the step to make sure that there's a y target there with our down speed.
-	upRights := make(map[int]map[int]map[int]struct{})
+	upRights := make(map[int]map[int]struct{})
 
-	for i := 1; i <= maxInitialVelocity; i++ {
-		diff := i * 2
+	for i := -1; i >= maxInitialVelocity; i-- {
+		diff := i * -2
 
 		for speed, steps := range speedsSteps {
 			if len(steps) < 2 {
@@ -162,67 +155,34 @@ func getRightUpShotsNotScrubbed(
 			}
 
 			for step := range steps {
-				if _, ok := steps[step-diff]; ok {
-					if upRights[speed] == nil {
-						upRights[speed] = make(map[int]map[int]struct{})
-					}
-
-					if upRights[speed][step-diff] == nil {
-						upRights[speed][step-diff] = make(map[int]struct{})
-					}
-
-					upRights[speed][step-diff][i] = struct{}{}
-
-					if firingSolutions[step-diff] == nil {
-						firingSolutions[step-diff] = make(map[int]struct{})
-					}
-
-					firingSolutions[step-diff][i] = struct{}{}
+				if _, ok := steps[step-diff]; !ok {
+					continue
 				}
-			}
-		}
-	}
 
-	crosschecked := make(map[int]map[int]struct{})
-
-	fmt.Printf("" +
-		"-----------------------\n" +
-		"-- checking uprights --\n" +
-		"-----------------------\n\n")
-	for speed, steps := range upRights {
-		fmt.Printf("checking speeds %d\n", speed)
-		for step, initialVelocities := range steps {
-			fmt.Printf("- checking for step %d, velocities: %v\n", step, initialVelocities)
-			for initialVelocity := range initialVelocities {
-				fmt.Printf("- - checking initialvelocity: %d\n", initialVelocity)
-				if _, ok := yTargets[step]; ok {
-					fmt.Printf("- - - ytargets had step %d in it with speeds: %v\n", step, yTargets[step])
-					for _, yspeed := range yTargets[step] {
-						if yspeed == -initialVelocity {
-							if firingSolutions[speed] == nil {
-								firingSolutions[speed] = make(map[int]struct{})
+				if velocities, ok := yTargets[step-diff]; ok {
+					for _, v := range velocities {
+						if v == i {
+							if upRights[speed] == nil {
+								upRights[speed] = make(map[int]struct{})
 							}
 
-							firingSolutions[speed][initialVelocity] = struct{}{}
-
-							if crosschecked[speed] == nil {
-								crosschecked[speed] = make(map[int]struct{})
-							}
-
-							crosschecked[speed][initialVelocity] = struct{}{}
+							upRights[speed][-v] = struct{}{}
 						}
 					}
-				} else {
-					fmt.Printf("- - - ytargets did not have step %d in it\n", step)
 				}
 			}
 		}
 	}
 
-	fmt.Printf("crosschecked\n\n%#v\n\n", crosschecked)
+	for x, ys := range upRights {
+		for y := range ys {
+			if firingSolutions[x] == nil {
+				firingSolutions[x] = make(map[int]struct{})
+			}
 
-	//fmt.Printf("new uprights\n\n%#v\n\n", upRights)
-	//fmt.Printf("speeds step slice:\n\n%#v\n\n", speedStepsSlice)
+			firingSolutions[x][y] = struct{}{}
+		}
+	}
 
 	return firingSolutions
 }
@@ -242,82 +202,6 @@ func getDirectShots(xMin, xMax, yMin, yMax int) [][2]int {
 
 	return vecs
 }
-
-// getHorizontalZeroSpeedFromAbove assumes that the horizontal speed of the projectile has been scrubbed away.
-//func getHorizontalZeroSpeedFromAbove(xMin, xMax, yMin, yMax int) [][2]int {
-//	x1 := math.Ceil((-1 + math.Sqrt(1-4*1*-(2*float64(xMin)))) / 2)
-//	x2 := math.Floor((-1 + math.Sqrt(1-4*1*-(2*float64(xMax)))) / 2)
-//
-//	exes := make([]int, 0)
-//	for i := x1; i <= x2; i++ {
-//		exes = append(exes, int(i))
-//	}
-//	fmt.Printf("solving for quadrating, x1 is %.4f, x2 is %.4f, exes is %v\n", x1, x2, exes)
-//
-//	// yMin is the lower bound of the target area, the bottom, ie the one further away from the zero line, ie the larger
-//	// number in absolute value.
-//
-//	yMin = -yMin
-//	yMax = -yMax
-//
-//	sums := firstNNumbers(yMax)
-//	fmt.Printf("\n\n\n%#v\n\n\n", sums)
-//	//
-//
-//	mostSteps := 0
-//
-//	for k, v := range sums {
-//		if v > yMin {
-//			break
-//		}
-//
-//		mostSteps = k
-//	}
-//
-//	fmt.Printf("most steps to reach ymin (%d) is %d with value %d\n", yMin, mostSteps, sums[mostSteps])
-//
-//	diff := yMin - yMax
-//	// for any value above the minimum number needed to shed all horizontal speed
-//	for i := int(x1); i <= diff; i++ {
-//		for j := 1; j <= mostSteps; j++ {
-//
-//		}
-//	}
-//
-//	fmt.Printf("ymin: %d, ymax: %d, diff: %d\n", yMin, yMax, diff)
-//
-//	// for the ys we need to range over essentially all the y coordinates. As long as the smallest possible y is less
-//	// than the number of steps it would take for x to scrub its speed, we're good. Because each step it loses one
-//	// unit of speed, the minimum number of steps is x1.
-//	//
-//	// I also subtract one, because we need the previous step, and minus it because we're shooting up.
-//	//whys := make([]int, 0)
-//	//for i := yMin; i <= yMax; i++ {
-//	//	whys = append(whys, -(i + 1))
-//	//}
-//
-//	// Need to collect all the consecutive numbers that add up to anywhere in the range, provided the number is bigger
-//	// than (x1+1)/2.
-//	//for i := 1; i <= yMax/2; i++ {
-//	//	fmt.Printf("trying with i = %d\n", i)
-//	//	step := float64(yMax) / float64(i)
-//	//	fmt.Printf("found mid: %.4f\n", step)
-//	//	from := int(math.Floor(step - 1/float64(i)))
-//	//	fmt.Printf("minus half of i, flooring it, and inting that: %d\n", from)
-//	//
-//	//	acc := 0
-//	//
-//	//	for j := 0; j < i; j++ {
-//	//		fmt.Printf("-- adding consecutive numbers: %d\n", from+j)
-//	//		acc += from + j
-//	//	}
-//	//	fmt.Printf("doing a check with accumulating i (%d) successives: %d\n", i, acc)
-//	//}
-//
-//	//fmt.Printf("the whys: %v\n", whys)
-//
-//	return nil
-//}
 
 func firstNNumbers(max int) []int {
 	sums := make([]int, max+1)
@@ -387,10 +271,6 @@ func getYTargetSpeeds(yMin, yMax int) map[int][]int {
 		maxNumber = stepDifference
 	}
 
-	//fmt.Printf("yMin: %d, yMax: %d, max: %d\n", yMin, yMax, maxNumber)
-
-	// starter + number of steps - sum(starter-1)
-
 	// starter
 	for i := 0; i <= yMax; i++ {
 		// number of steps
@@ -405,8 +285,6 @@ func getYTargetSpeeds(yMin, yMax int) map[int][]int {
 				break
 			}
 
-			//fmt.Printf("starter: %d, steps: %d, sum: %d, sumprevious: %d, blergh: %d\n", i, j, sums[i+j-1], previous, blergh)
-
 			if blergh >= yMin {
 				if velocities[j] == nil {
 					velocities[j] = make([]int, 0)
@@ -416,30 +294,6 @@ func getYTargetSpeeds(yMin, yMax int) map[int][]int {
 			}
 		}
 	}
-
-	//
-	//for stepDifference := 0; stepDifference <= maxNumber; stepDifference++ {
-	//	fmt.Printf("iterating outer: %d\n", stepDifference)
-	//
-	//	for initialVelocity := range sums {
-	//		endsUp := sums[initialVelocity+stepDifference]
-	//		fmt.Printf("-- iterating inner initvelocity: %d, endsUp: %d\n", initialVelocity, endsUp)
-	//
-	//		// Too late, everything past this point for this step difference is going to be too big, no point in
-	//		// checking further.
-	//		if endsUp > yMax {
-	//			break
-	//		}
-	//
-	//		if endsUp >= yMin && endsUp <= yMax {
-	//			if velocities[stepDifference] == nil {
-	//				velocities[stepDifference] = make([]int, 0)
-	//			}
-	//
-	//			velocities[stepDifference] = append(velocities[stepDifference], initialVelocity)
-	//		}
-	//	}
-	//}
 
 	return velocities
 }
