d81688d06872f443e25bda6275cc67d0be9d89d5
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-21 14:13:27 +0000

    Add split command

--- day18/common.go
+++ day18/common.go
@@ -8,6 +8,17 @@ import (
 	"github.com/javorszky/adventofcode2021/util"
 )
 
+const (
+	notLeafError nodeError = "not a Leaf, can't split"
+	valueTooLow  nodeError = "value too low to split"
+)
+
+type nodeError string
+
+func (n nodeError) Error() string {
+	return string(n)
+}
+
 // getInputs reads the input.txt file and returns them as a slice of strings for each row.
 func getInputs(fn string) []string {
 	data, err := ioutil.ReadFile(fn)
@@ -44,6 +55,41 @@ func (n *node) Parent() *node {
 
 func (n *node) Depth() int {
 	return n.depth
+}
+
+func (n *node) Split() error {
+	if n.left != nil || n.right != nil {
+		return notLeafError
+	}
+
+	if n.value < 10 {
+		return valueTooLow
+	}
+
+	l := n.value / 2
+	r := n.value - l
+
+	leftNode := &node{
+		parent: n,
+		value:  l,
+		left:   nil,
+		right:  nil,
+		depth:  n.depth + 1,
+	}
+
+	rightNode := &node{
+		parent: n,
+		value:  r,
+		left:   nil,
+		right:  nil,
+		depth:  n.depth + 1,
+	}
+
+	n.value = 0
+	n.left = leftNode
+	n.right = rightNode
+
+	return nil
 }
 
 func leaf(value int, parent *node) *node {
