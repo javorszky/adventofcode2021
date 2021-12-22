f146f994eb3d10e25430fa279fdfca2104be475e
Gabor Javorszky <gabor.javorszky@ardanlabs.com>
2021-12-22 22:52:06 +0000

    Day 18 task 2 done

--- day18/main.go
+++ day18/main.go
@@ -11,7 +11,7 @@ func Tasks() {
 
 	output := task1(input)
 	fmt.Printf("Task 1: The magnitude of the added together snailfish number is %d\n", output)
-	//
-	//output2 := task2(input)
-	//fmt.Printf("Task 2: The result is something: %v\n", output2)
+
+	output2 := task2(input)
+	fmt.Printf("Task 2: The largest value I can get is %d\n", output2)
 }
--- day18/task2.go
+++ day18/task2.go
@@ -1,5 +1,24 @@
 package day18
 
-func task2(input []string) interface{} {
-	return input
+func task2(input []string) int {
+	max := 0
+
+	for i, vs := range input {
+		for j, ws := range input {
+			if i == j {
+				continue
+			}
+
+			v := parse(vs)
+			w := parse(ws)
+			wv := addNodes(v, w)
+			wvMag := wv.Magnitude()
+
+			if max < wvMag {
+				max = wvMag
+			}
+		}
+	}
+
+	return max
 }
