10c99526c9d42977ea2f38c1ec7ee3d830e30bee
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-23 15:37:41 +0000

    Beacons are now sortable

--- day19/probe.go
+++ day19/probe.go
@@ -1,14 +1,34 @@
 package day19
 
 import (
+	"sort"
 	"strings"
 
 	"github.com/javorszky/adventofcode2021/util"
 )
 
 type probe struct {
-	name    string
-	beacons []position
+	name      string
+	beacons   []position
+	distances []int
+}
+
+type beacons []position
+
+func (b beacons) Len() int      { return len(b) }
+func (b beacons) Swap(i, j int) { b[i], b[j] = b[j], b[i] }
+func (b beacons) Less(i, j int) bool {
+	// true if I should come before j
+	// if x is clearly smaller, return that
+	if b[i].x == b[j].x {
+		if b[i].y == b[j].y {
+			return b[i].z < b[j].z
+		}
+
+		return b[i].y < b[j].y
+	}
+
+	return b[i].x < b[j].x
 }
 
 func parseProbes(input string) []probe {
@@ -32,4 +52,37 @@ func parseProbes(input string) []probe {
 	}
 
 	return probes
+}
+
+func parseDistances(beaconSlice beacons) []int {
+	distances := make([]int, 0, (len(beaconSlice)*(len(beaconSlice)-1))/2)
+
+	sort.Sort(beaconSlice)
+
+	for i, beacon := range beaconSlice {
+		for _, otherBeacon := range beaconSlice[i+1:] {
+			distances = append(distances, distance(beacon, otherBeacon))
+		}
+	}
+
+	sort.Ints(distances)
+
+	return nil
+}
+
+func findLowestPoint(beacons []position) position {
+	xes := make([]int, len(beacons))
+	for i, beacon := range beacons {
+		xes[i] = beacon.x
+	}
+
+	sort.Ints(xes)
+
+	lowestBeacons := make([]position, 0)
+	for _, beacon := range beacons {
+		if beacon.x == xes[0] {
+			lowestBeacons = append(lowestBeacons, beacon)
+		}
+	}
+
 }
