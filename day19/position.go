6d9e37052b258207c9f52723f03eaada04c66e1b
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-23 11:46:08 +0000

    Rotations are now done

--- day19/position.go
+++ day19/position.go
@@ -45,35 +45,41 @@ func (p position) String() string {
 
 func (p position) rotations() [24]position {
 	return [24]position{
+		// Double checked
 		{x: p.x, y: p.y, z: p.z},   // xpzp, self
-		{x: p.x, y: p.z, z: -p.y},  // xpym
+		{x: p.x, y: -p.z, z: p.y},  // xpyp
 		{x: p.x, y: -p.y, z: -p.z}, // xpzm (z minus also flips y by 180)
-		{x: p.x, y: p.z, z: -p.y},  // xpyp
+		{x: p.x, y: p.z, z: -p.y},  // xpym
 
+		// Double checked
 		{x: -p.x, y: -p.y, z: p.z},  // xmzp
 		{x: -p.x, y: -p.z, z: -p.y}, // xmym
 		{x: -p.x, y: p.y, z: -p.z},  // xmzm
 		{x: -p.x, y: p.z, z: p.y},   // xmyp
 
-		{x: p.y, y: -p.x, z: p.z},  // ypzp
-		{x: p.y, y: -p.z, z: -p.x}, // ypxm
+		// Double checked
+		{x: -p.y, y: p.x, z: p.z},  // ypzp
+		{x: -p.z, y: p.x, z: -p.y}, // ypxm
 		{x: p.y, y: p.x, z: -p.z},  // ypzm
-		{x: p.y, y: p.z, z: p.x},   // ypxp
+		{x: p.z, y: p.x, z: p.y},   // ypxp
 
-		{x: -p.y, y: p.x, z: p.z},   // ymzp
-		{x: -p.y, y: -p.z, z: p.x},  // ymxm
+		// Double checked
+		{x: p.y, y: -p.x, z: p.z},   // ymzp
+		{x: -p.z, y: -p.x, z: p.y},  // ymxm
 		{x: -p.y, y: -p.x, z: -p.z}, // ymzm
-		{x: -p.z, y: p.z, z: -p.x},  // ymxp
+		{x: p.z, y: -p.x, z: -p.y},  // ymxp
 
-		{x: p.z, y: p.y, z: -p.x},  // zpxm
-		{x: p.z, y: p.x, z: p.y},   //zpym
+		// Double checked
+		{x: -p.z, y: p.y, z: p.x},  // zpxm
+		{x: -p.y, y: -p.z, z: p.x}, // zpym
 		{x: p.z, y: -p.y, z: p.x},  // zpxp
-		{x: p.z, y: -p.x, z: -p.y}, // zpyp
+		{x: p.y, y: p.z, z: p.x},   // zpyp
 
-		{x: -p.z, y: p.y, z: p.x},   // zmxp
-		{x: -p.z, y: -p.x, z: p.y},  // zmym
+		// Double checked
+		{x: p.z, y: p.y, z: -p.x},   // zmxp
+		{x: -p.y, y: p.z, z: -p.x},  // zmyp
 		{x: -p.z, y: -p.y, z: -p.x}, // zmxm
-		{x: -p.z, y: p.x, z: -p.y},  // zmyp
+		{x: p.y, y: -p.z, z: -p.x},  // zmym
 	}
 }
 
