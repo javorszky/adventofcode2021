0e5b58b4c5d5a09a7fc11f8f7b9c3f8c1293dc09
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-26 13:13:55 +0000

    Day 20 task 2 done

--- day20/task2.go
+++ day20/task2.go
@@ -1,5 +1,11 @@
 package day20
 
-func task2(enhance, image string) interface{} {
-	return 1
+func task2(enhance, image string) int {
+	img := NewImage(image, enhance)
+
+	for i := 0; i < 50; i++ {
+		img = img.tick()
+	}
+
+	return img.Lights()
 }
