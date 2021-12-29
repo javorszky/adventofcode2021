38a736722e6a3fdea9c84f96d9a7a0c6ecf7105a
Gabor Javorszky <gabor@javorszky.co.uk>
2021-12-29 23:35:18 +0000

    Fix top back edge tests

--- day22/instruction_test.go
+++ day22/instruction_test.go
@@ -1197,10 +1197,44 @@ func Test_findTopBackEdge(t *testing.T) {
 			want: []instruction{
 				{
 					xFrom: -20,
-					xTo:   -10,
+					xTo:   -11,
 					yFrom: -10,
 					yTo:   10,
-					zFrom: 10,
+					zFrom: 11,
+					zTo:   20,
+					flip:  off,
+				},
+			},
+		},
+		{
+			name: "returns single line top right edge box",
+			args: args{
+				box: instruction{
+					xFrom: -20,
+					xTo:   20,
+					yFrom: -20,
+					yTo:   20,
+					zFrom: -20,
+					zTo:   20,
+					flip:  off,
+				},
+				overlapBox: instruction{
+					xFrom: -19,
+					xTo:   10,
+					yFrom: -10,
+					yTo:   10,
+					zFrom: -10,
+					zTo:   19,
+					flip:  on,
+				},
+			},
+			want: []instruction{
+				{
+					xFrom: -20,
+					xTo:   -20,
+					yFrom: -10,
+					yTo:   10,
+					zFrom: 20,
 					zTo:   20,
 					flip:  off,
 				},
